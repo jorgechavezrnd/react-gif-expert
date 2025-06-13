e595c43ffd52808ef036f17b6dc456e0b1a0555b
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-13 00:54:57 -0400

    Add tests for not call onNewCategory in some scenarios

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -17,9 +17,9 @@ describe('Pruebas en <AddCategory />', () => {
   test('debe de llamar onNewCategory si el input tiene un valor', () => {
 
     const inputValue = 'Saitama';
-    // TODO: ????
+    const onNewCategory = jest.fn();
 
-    render( <AddCategory onNewCategory={ () => {} } /> );
+    render( <AddCategory onNewCategory={ onNewCategory } /> );
 
     const input = screen.getByRole('textbox');
     const form = screen.getByRole('form');
@@ -28,6 +28,23 @@ describe('Pruebas en <AddCategory />', () => {
     fireEvent.submit( form );
     // screen.debug();
     expect( input.value ).toBe('');
+
+    expect( onNewCategory ).toHaveBeenCalled();
+    expect( onNewCategory ).toHaveBeenCalledTimes(1);
+    expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+
+  });
+
+  test('no debe de llamar el onNewCategory si el input esta vacio', () => {
+
+    const onNewCategory = jest.fn();
+    render( <AddCategory onNewCategory={ onNewCategory } /> );
+
+    const form = screen.getByRole('form');
+    fireEvent.submit( form );
+
+    expect( onNewCategory ).toHaveBeenCalledTimes(0);
+    expect( onNewCategory ).not.toHaveBeenCalled();
 
   });
 
